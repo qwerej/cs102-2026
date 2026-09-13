27250d4d3428660fab8cce93368890afb19aa7b0
Veronika <veronikaavd@icloud.com>
2026-09-13 13:28:17 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,8 @@
+""" Присваиваем переменной message значение"""
+
+message = "Hello,world"
 def text():
+    """Возвращает приветственное сообщение и выводит его на экран."""
+
     return message
+print(message)
